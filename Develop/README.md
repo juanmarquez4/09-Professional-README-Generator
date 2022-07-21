74eeeca845f749d673e04c11e12386b587980e8c
Juan Marquez <juanmarquez4@gmail.com>
2022-07-21 15:23:32 +1000

    Updated Readme Generator

--- Develop/README.md
+++ Develop/README.md
@@ -1,5 +1,5 @@
 
-# jahdjkh
+# Professional Readme Generator   [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
 
 ## Table of Contents
 1. [Description](#description)
@@ -11,22 +11,23 @@
 7. [Questions](#questions)
 
 ### Description
-alsddlk
+This project can quickly and easily create a README file by using a command-line application to generate one
 
 ### Instalation
-ksadlkj
+This app will create a command-line application that dynamically generates a professional README.md file from a user’s input using the Inquirer package.
 
 ### Usage
-lkjasdlkj
+The README Generator can be use when creating a project as to include a document which informs what the app is for, how to use the app, how to install it, how to report issues and how to make contribution.
 
 ### License
-Unlicense
+#### Unlicense:
+A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.
 
 ### Contributing:
-lkjsdklj
+To contribute follow instruction on Professional README Guide as a reminder of everything that a high-quality, professional README should contain.
 
 ### Testing
-lkjsadlkj
+This application will be invoked by using node index.js
 
 ### Questions:
 For additional questions about the project please follow the link to GitHub profile or send an email
--- Develop/index.js
+++ Develop/index.js
@@ -52,15 +52,37 @@ const questions = () => {
             message: 'Enter your email'
         },
     ])
-    .then((response) =>
-    console.log(response.license)
-    )
 };
+
+// Gene
+const generateLicense = (l) => {
+    if (l == "Unlicense")
+    return "A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code."
+    else if (l == "MIT License")
+    return "A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code."
+    else if (l == "Mozilla Public License")
+    return "Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work."
+    else if (l == "GNU AGPLv3")
+    return "Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available."
+    return l  
+}
+
+const licenseBadge = (l) => {
+    if (l == "Unlicense")
+    return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+    else if (l == "MIT License")
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    else if (l == "Mozilla Public License")
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+    else if (l == "GNU AGPLv3")
+    return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)"
+    return l  
+}
 
 // TODO: Create a function to write README file
 const generateREADME = ({title, description, instruction, information, guidelines, test, license, github, email }) =>
 `
-# ${title}
+# ${title}   ${licenseBadge(license)}
 
 ## Table of Contents
 1. [Description](#description)
@@ -81,7 +103,8 @@ ${instruction}
 ${information}
 
 ### License
-${license}
+#### ${license}:
+${generateLicense(license)}
 
 ### Contributing:
 ${guidelines}
